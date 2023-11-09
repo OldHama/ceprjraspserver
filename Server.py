736fcc3cb7613b8103e6df8cfa4ddaa9901ab6b4
Seo Myeong Ho <86174016+OldHama@users.noreply.github.com>
2023-11-10 03:19:09 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import logging
 from datetime import datetime
+
 
 app = Flask(__name__)
 my_serial_num = 202311080001
@@ -16,37 +17,33 @@ app.logger.addHandler(file_handler)
 def hello():
     return "hello"
 
-@app.route('/make_cocktail/<string:UserID>/<string:cocktail_name>/<int:first>/<int:second>/<int:third>/<int:fourth>', methods=['POST'])
-def make_cocktail(UserID, cocktail_name, first, second, third, fourth):
-    current_date = datetime.now().strftime("%Y%m%d")
+@app.route('/make_cocktail', methods=['POST', 'GET'])
+def make_cocktail():
+    data = request.json
+    
+    if data:
+        UserID = data.get('UserID')
+        cocktail_name = data.get('cocktail_name')
+        first = data.get('first')
+        second = data.get('second')
+        third = data.get('third')
+        fourth = data.get('fourth')
 
-    if request.method == 'POST':
-        # POST 요청의 데이터를 JSON 형식으로 파싱
-        data = request.json
-        
-        if data:
-            # 요청에서 받은 데이터를 이용하여 응답 생성
-            if 'UserID' in data:
-                UserID = data['UserID']
-            if 'cocktail_name' in data:
-                cocktail_name = data['cocktail_name']
-            if 'first' in data:
-                first = data['first']
-            if 'second' in data:
-                second = data['second']
-            if 'third' in data:
-                third = data['third']
-            if 'fourth' in data:
-                fourth = data['fourth']
-
-            if cocktail_name == ' ':
-                return f"UserID: {UserID} Cocktail Name: CustomCocktail\nFirst: {first},\nSecond: {second},\nThird:{third},\nFourth:{fourth} date: {current_date}"
-            else:
-                return f"UserID: {UserID} Cocktail Name: {cocktail_name}\nFirst: {first},\nSecond: {second},\nThird:{third},\nFourth:{fourth} date: {current_date}"
+        if all([UserID, cocktail_name, first, second, third, fourth]):
+            current_date = datetime.now().strftime("%Y%m%d")
+            return jsonify({
+                'UserID': UserID,
+                'cocktail_name': cocktail_name,
+                'first': first,
+                'second': second,
+                'third': third,
+                'fourth': fourth
+            })
         else:
-            return "POST 요청에 유효한 JSON 데이터가 필요합니다.", 400  # 잘못된 요청에 대한 응답
+            return "Invalid JSON data", 400
     else:
-        return "GET 요청은 지원하지 않습니다.", 405  # 허용되지 않는 메서드에 대한 응답
+        return "JSON data is required", 400
 
 if __name__ == '__main__':
     app.run(host='192.168.0.104', port=10000)
+
