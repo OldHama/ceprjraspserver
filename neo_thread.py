fe1973802c724e00696ccb7235b132e0e5873e2e
Seo Myeong Ho <86174016+OldHama@users.noreply.github.com>
2023-11-29 00:59:20 +0900

    Update neo_thread.py

--- neo_thread.py
+++ neo_thread.py
@@ -1,6 +1,9 @@
 import threading
 import neo_act as n
 import actuator
+import random as rnd
+
+message = ''
 
 class led_thread(threading.Thread):
     def __init__(self, name):
@@ -13,7 +16,7 @@ class led_thread(threading.Thread):
         
     def read_profile(self):
         try:
-            file = open("profile.txt", 'r')
+            file = open("/home/cocktaillove/maker_python_server/profile.txt", 'r')
             pattern_name = file.readlines()
             file.close()
             if not pattern_name:
@@ -26,12 +29,14 @@ class led_thread(threading.Thread):
             self.read_profile(self)
     
     def write_profile(self, msg):
+        global message
+        message = msg
         try:
-            file = open("profile.txt", "w")
+            file = open("/home/cocktaillove/maker_python_server/profile.txt", "w")
             file.write(msg)
             file.close()
         except:
-            self.write_profile(self, msg)
+            self.write_profile(self, message)
         
     def run (self):
         while True:
@@ -40,7 +45,10 @@ class led_thread(threading.Thread):
                 n.rainbow_cycle(self.timing, self.bright)
             elif self.pattern == 'breathe':
                 n.breathe(self.timing, self.bright)
+            elif self.pattern == 'chasing':
+                n.chase((255, 255, 255), self.timing, self.bright)
+            elif self.pattern == 'sparkle':
+                n.sparkle((rnd.randint(0, 255),rnd.randint(0, 255),rnd.randint(0, 255)), self.timing, self.bright)
             else:
                 n.off()
             # actuator.sleep(0.01)
-
